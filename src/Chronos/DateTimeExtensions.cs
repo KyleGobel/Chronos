3d992fd130d1f3533ce672f29f01808eea5bfb0b
Kyle Gobel <kgobel@deerso.com>
2014-03-17 16:53:54 -0400

    added startOfDay and EndOfDay datetime extensions

---
+++ src/Chronos/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chronos
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime StartOfDay(this DateTime date)
+        {
+            return date.Date;
+        }
+
+        public static DateTime EndOfDay(this DateTime date)
+        {
+            return date.Date.AddDays(1).AddTicks(-1);
+        }
+         
+    }
+}
