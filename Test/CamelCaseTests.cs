cd2bc8a1eef5e9d77cb9973a7db3209f2088d820
Kyle Gobel <kgobel@deerso.com>
2014-03-18 12:41:42 -0400

    added camelCase extension and a test proj

---
+++ Test/CamelCaseTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chronos;
+using Xunit;
+
+namespace Test
+{
+    [Trait("String Extensions", "Camel Case")]
+    public class CamelCaseTests
+    {
+        [Fact (DisplayName = "Converts a normal string with spaces")]
+        public void CanConvertStringToCamelCase()
+        {
+            const string testString = "well this should be easy";
+            var result = testString.ToCamelCase();
+            Assert.Equal("wellThisShouldBeEasy", result);
+        }
+
+        [Fact (DisplayName = "Returns an empty string when the input is null or empty")]
+        public void EmptyOrNullStringReturnsEmptyString()
+        {
+            const string testString = "";
+            const string testString2 = null;
+
+            var result1 = testString.ToCamelCase();
+            var result2 = testString2.ToCamelCase();
+
+            Assert.Equal(string.Empty, result1);
+            Assert.Equal(string.Empty, result2);
+        }
+    }
+}
---
+++ src/Chronos/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Chronos
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string This)
+        {
+            if (string.IsNullOrEmpty(This))
+                return string.Empty;
+            var info = new CultureInfo("en-US");
+            return info.TextInfo.ToTitleCase(This).Replace(" ", "");
+        }
+
+        public static string ToCamelCase(this string This)
+        {
+            var str = This.ToTitleCase();
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+            var firstPart = str[0].ToString(CultureInfo.InvariantCulture).ToLower();
+            var secondPart = str.Substring(1);
+
+            return string.Join("", firstPart, secondPart);
+        }
+
+        public static bool IsBlank(this string This)
+        {
+            return string.IsNullOrWhiteSpace(This);
+        }
+    }
+}
