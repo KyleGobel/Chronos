a773391714a10d8f02228dfb97ee4b9c774e4f61
Kyle Gobel <kgobel@gmail.com>
2014-03-17 22:47:52 -0400

    added some enumerable extensions

---
+++ src/Chronos/EnumerableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chronos
+{
+    public static class EnumerableExtensions
+    {
+
+        //thanks to jon skeet for these 
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (action == null) throw new ArgumentNullException("action");
+            foreach (var element in source)
+            {
+                action(element);
+            }
+        }
+
+        //meh no null checking or option to set ur own comparer, maybe later
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+     (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var knownKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (knownKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+         
+    }
+}
