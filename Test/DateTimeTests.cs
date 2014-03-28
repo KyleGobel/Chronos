010fc57b03eda124594e5e0f603a9af2c59d2109
Kyle Gobel <kgobel@gmail.com>
2014-03-27 20:36:23 -0400

    few mroe tests

--- Test/DateTimeTests.cs
+++ Test/DateTimeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using Chronos;
 using Xunit;
 
@@ -7,48 +8,113 @@ namespace Test
     [Trait("DateTime Extensions", "")]
     public class DateTimeTests
     {
-        [Fact(DisplayName = "Start of day returns start of day")]
-        public void StartOfDay()
-        {
-            var anyDate = new DateTime(2014, 1, 3, 3, 24, 50);
-            var expectedResult = new DateTime(anyDate.Year, anyDate.Month, anyDate.Day, 0, 0, 0);
 
-            Assert.Equal(expectedResult, anyDate.StartOfDay());
+        [Trait("DateTime Extensions", "StartOfDay")]
+        public class StartOfDayMethod
+        {
+             [Fact(DisplayName = "Start of day returns start of day")]
+            public void StartOfDay()
+            {
+                var anyDate = new DateTime(2014, 1, 3, 3, 24, 50);
+                var expectedResult = new DateTime(anyDate.Year, anyDate.Month, anyDate.Day, 0, 0, 0);
+
+                Assert.Equal(expectedResult, anyDate.StartOfDay());
+            }   
         }
 
-        [Fact(DisplayName = "EndOfDay returns the end of the day")]
-        public void EndOfDay()
+        [Trait("DateTime Extensions", "EndOfDay")]
+        public class EndOfDayMethod
         {
-            var anyDate = new DateTime(2014, 1, 3, 3, 24, 50);
-            var expectedResult = DateTime.Parse(
-                string.Format(
-                    "{0}-{1}-{2} {3}",
-                    anyDate.Year,
-                    anyDate.Month,
-                    anyDate.Day,
-                    "23:59:59.9999999"));
+            [Fact(DisplayName = "EndOfDay returns the end of the day")]
+            public void EndOfDay()
+            {
+                var anyDate = new DateTime(2014, 1, 3, 3, 24, 50);
+                var expectedResult = DateTime.Parse(
+                    string.Format(
+                        "{0}-{1}-{2} {3}",
+                        anyDate.Year,
+                        anyDate.Month,
+                        anyDate.Day,
+                        "23:59:59.9999999"));
 
 
-            Assert.Equal(expectedResult,anyDate.EndOfDay());
+                Assert.Equal(expectedResult, anyDate.EndOfDay());
+            }
         }
 
-        [Fact(DisplayName = "Converts to unix timestamp")]
-        public void ToUnixTimeStampTest()
-        {
-            var testDate = new DateTime(2014, 1, 1);
-            const long expectedTimestamp = 1388534400;
 
-            Assert.Equal(expectedTimestamp, testDate.ToUnixTimestamp());
+
+        [Trait("DateTime Extensions", "Unix Timestamps")]
+        public class UnixTimestampMethods
+        {
+            [Fact(DisplayName = "Converts to unix timestamp")]
+            public void ToUnixTimeStampTest()
+            {
+                var testDate = new DateTime(2014, 1, 1);
+                const long expectedTimestamp = 1388534400;
+
+                Assert.Equal(expectedTimestamp, testDate.ToUnixTimestamp());
+
+            }
+
+            [Fact(DisplayName = "Converts a timestamp to System.DateTime")]
+            public void FromUnixTimeStampTest()
+            {
+                const long testTimestamp = 1388534400;
+                var expectedDate = new DateTime(2014, 1, 1);
+
+                Assert.Equal(expectedDate, testTimestamp.FromUnixTimestamp());
+            }
+        }
+
+        [Trait("DateTime Extensions", "IsBetween")]
+        public class IsBetweenMethod 
+        {
+            [Fact(DisplayName = "Returns true when between two days")]
+            public void IsBetweenValues()
+            {
+                DateTime start = new DateTime(2010, 1, 1);
+                DateTime end = new DateTime(2010, 1, 3);
+
+                DateTime testValue = new DateTime(2010, 1, 2);
+
+                Assert.True(testValue.IsBetween(start, end));
+            }
+
+            [Fact(DisplayName = "Returns false when not between two days")]
+            public void IsNotBetweenValues()
+            {
+                DateTime start = new DateTime(2010, 1, 1);
+                DateTime end = new DateTime(2010, 1, 3);
+
+                DateTime testValue = new DateTime(2010, 1, 4);
+
+                Assert.False(testValue.IsBetween(start, end));               
+            }
+
+            [Fact(DisplayName = "Returns true when between two times")]
+           public void BetweenTimes() 
+            {
+                DateTime start = new DateTime(2010, 1, 1, 1,0,0);
+                DateTime end = new DateTime(2010, 1, 1, 3, 0, 0);
+
+                DateTime testValue = new DateTime(2010, 1, 1, 2, 0, 0);
+
+                Assert.True(testValue.IsBetween(start, end, true));                              
+            }
+
+            [Fact(DisplayName = "Returns false when not between two times")]
+            public void IsFalseWhenNotSpecifiedTime()
+            {
+                DateTime start = DateTime.Parse("2010-1-1 1:30:00");
+                DateTime end = DateTime.Parse("2010-1-1 3:00 AM");
+
+                DateTime testValue = DateTime.Parse("2010-1-1 4:00 AM");
+
+                Assert.False(testValue.IsBetween(start, end, true));                                             
+            }
 
         }
 
-        [Fact(DisplayName = "Converts a timestamp to System.DateTime")]
-        public void FromUnixTimeStampTest()
-        {
-            const long testTimestamp = 1388534400;
-            var expectedDate = new DateTime(2014, 1, 1);
-
-            Assert.Equal(expectedDate, testTimestamp.FromUnixTimestamp());
-        }
     }
 }
