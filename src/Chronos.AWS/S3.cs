81b65881db3c3fd623a0ac2ce71ef1a85a81f7be
Kyle Gobel <kgobel@gmail.com>
2014-08-31 00:40:35 -0400

    added aws package
    Added a new nuget pkg for AWS related items.   This package has a
    dependency on AWSSDK

---
+++ src/Chronos.AWS/S3.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Chronos.Configuration;
+
+namespace Chronos.AWS
+{
+    public class S3
+    {
+        private readonly S3ConnectionString _connectionInfo;
+        public S3(S3ConnectionString connectionString)
+        {
+            _connectionInfo = connectionString;
+        }
+
+        public S3(string connectionStringOrAppSettingName)
+        {
+            S3ConnectionString connStr;
+            if (S3ConnectionString.TryParse(connectionStringOrAppSettingName, out connStr))
+            {
+                _connectionInfo = connStr;
+            }
+            else
+            {
+                _connectionInfo = ConfigUtilities.GetS3ConnectionString(connectionStringOrAppSettingName);
+            }
+        }
+
+        public string PutFile(string filepath, string saveAs)
+        {
+            return PutFile(filepath, saveAs, S3CannedACL.PublicRead);
+        }
+        public string PutFile(string filepath, string saveAs, S3CannedACL acl)
+        {
+            using (var client = new AmazonS3Client(_connectionInfo.AccessKey, _connectionInfo.SecretKey))
+            {
+                var request = new PutObjectRequest
+                {
+                    BucketName = _connectionInfo.BucketName,
+                    FilePath = filepath,
+                    CannedACL = acl,
+                    Key = _connectionInfo.FolderName + "/" + saveAs
+                };
+
+                client.PutObject(request);
+                return string.Format("http://s3.amazonaws.com/{0}/{1}/{2}", _connectionInfo.BucketName,
+                    _connectionInfo.FolderName, saveAs);
+            }
+        }
+    }
+}
