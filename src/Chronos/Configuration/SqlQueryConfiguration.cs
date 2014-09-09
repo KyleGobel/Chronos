e459ed9341f27b1301232cd4498dde906cb30197
Kyle <kgobel@gmail.com>
2014-09-08 20:11:20 -0400

    added sql query extensions
    allows you to save sql queries as embedded resources

---
+++ src/Chronos/Configuration/SqlQueryConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace Chronos.Configuration
+{
+    public class SqlQueryConfiguration
+    {
+        public string StartDelimiter { get; set; }
+        public string QueriesNamespace { get; set; }
+        public Assembly Assembly { get; set; }
+    }
+}
--- src/Chronos/SqlQueryExtensions.cs
+++ src/Chronos/SqlQueryExtensions.cs
@@ -1,7 +1,42 @@
-﻿namespace Chronos
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Chronos.Configuration;
+
+namespace Chronos
 {
-    public class SqlQueryExtensions
+    public static class SqlQueryExtensions
     {
-         
+        public static string GetSqlQuery<T>(this T requestDto, string filename = null, Action<string> queryFilter = null, SqlQueryConfiguration config = null) where T : class
+        {
+            var sqlConfig = config ?? new SqlQueryConfiguration();
+            var namespacePart = sqlConfig.QueriesNamespace;
+
+            var sqlStatement = string.Empty;
+
+            //name is the same as the requestDto, but with a .sql extension
+            var resourceName = filename ?? (namespacePart + "." + typeof(T).Name + ".sql");
+
+            using (var stm = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                if (stm != null)
+                {
+                    sqlStatement = new StreamReader(stm).ReadToEnd();
+                }
+            }
+            var startIndex = sqlStatement.IndexOf(sqlConfig.StartDelimiter, StringComparison.CurrentCultureIgnoreCase);
+
+
+            if (startIndex > 0)
+            {
+                return sqlStatement.Substring(startIndex + sqlConfig.StartDelimiter.Length);
+            }
+
+            if (queryFilter != null)
+            {
+                queryFilter(sqlStatement);
+            }
+            return sqlStatement;
+        }
     }
 }
