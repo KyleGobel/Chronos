6e49a7953350a2e09809f175580df3a80e5f3a66
Kyle <kgobel@gmail.com>
2015-01-29 21:01:30 -0500

    added a simple loop processor
    this will help out with creating windows services with topshelf,
    everything here ready to go

---
+++ src/Chronos/ServiceProcessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chronos
+{
+    public abstract class LoopProcessor
+    {
+        public abstract TimeSpan TimeBetweenLoops { get; }
+        protected Task T;
+        protected CancellationTokenSource Ts;
+        protected CancellationToken CancellationToken;
+
+        protected Action<string> DebugLogAction;
+        protected Action<string, Exception> FatalLogAction;
+
+        protected LoopProcessor()
+        {
+            Ts = new CancellationTokenSource();
+            CancellationToken = Ts.Token;
+
+            T = new Task(() =>
+            {
+                for (;;)
+                {
+                    if (DebugLogAction != null) { DebugLogAction("Starting process loop"); }
+                    var startTime = Stopwatch.StartNew();
+                    try
+                    {
+                        ProcessLoop();
+                    }
+                    catch (Exception x)
+                    {
+                        if (FatalLogAction != null)
+                        {
+                            FatalLogAction("Fatal Error in Loop Processor", x);
+                        }
+                        Environment.Exit(-1);
+                    }
+
+                    if (DebugLogAction != null)
+                    {
+                        DebugLogAction(string.Format("Finished process loop in {0}, waiting {1}",
+                            startTime.Elapsed.ToString("g"), TimeBetweenLoops.ToString("g")));
+                    }
+
+                    Thread.Sleep(TimeBetweenLoops);
+                    if (CancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                }
+
+            }); 
+        }
+
+        public virtual void Start() { T.Start();}
+        public virtual void Stop() {Ts.Cancel();}
+        public abstract void ProcessLoop();
+    }
+}
