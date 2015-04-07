6e4b68a263c2a4a43f85de34a90bcc5dc30d98de
Kyle Gobel <kgobel@gmail.com>
2015-04-07 10:53:36 -0400

    made bucket a variable

--- src/Chronos.AWS/copyToRedshift.sql
+++ src/Chronos.AWS/copyToRedshift.sql
@@ -3,7 +3,7 @@
 create temp table $TABLENAME$_staging (like $TABLENAME$);
 
 copy $TABLENAME$_staging ($COLUMNLIST$)
-from 's3://mobi-email/$PATH$'
+from 's3://$BUCKET$/$PATH$'
 credentials 'aws_access_key_id=$ACCESSKEY$;aws_secret_access_key=$SECRETKEY$'
 delimiter '\t'
 ignoreheader as $HEADERROWS$
--- src/Chronos.AWS/mergeToRedshift.sql
+++ src/Chronos.AWS/mergeToRedshift.sql
@@ -1,5 +1,5 @@
 ﻿copy $TABLENAME$ ($COLUMNLIST$)
-from 's3://mobi-email/$PATH$'
+from 's3://$BUCKET$/$PATH$'
 credentials 'aws_access_key_id=$ACCESSKEY$;aws_secret_access_key=$SECRETKEY$'
 delimiter '\t'
 ignoreheader as $HEADERROWS$
