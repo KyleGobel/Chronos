4440efaf59fdcaf105c000e4fcc6db1555edf803
Kyle Gobel <kgobel@gmail.com>
2015-04-23 10:32:06 -0400

    had file names backwards :(((

--- src/Chronos.AWS/copyToRedshift.sql
+++ src/Chronos.AWS/copyToRedshift.sql
@@ -1,22 +1,6 @@
-﻿begin transaction;
-
-create temp table $TABLENAME$_staging (like $TABLENAME$);
-
-copy $TABLENAME$_staging ($COLUMNLIST$)
+﻿copy $TABLENAME$ ($COLUMNLIST$)
 from 's3://$BUCKET$/$PATH$'
 credentials 'aws_access_key_id=$ACCESSKEY$;aws_secret_access_key=$SECRETKEY$'
 delimiter '$DELIMETER$'
 ignoreheader as $HEADERROWS$
 ignoreblanklines BLANKSASNULL ACCEPTANYDATE TRUNCATECOLUMNS GZIP timeformat 'auto';
-
-delete from $TABLENAME$
-using $TABLENAME$_staging
-where
-$PRIMARYKEYCHECK$;
-
- insert into $TABLENAME$ 
- select * from $TABLENAME$_staging;
-
- drop table $TABLENAME$_staging;
-
- end transaction;
--- src/Chronos.AWS/mergeToRedshift.sql
+++ src/Chronos.AWS/mergeToRedshift.sql
@@ -1,6 +1,22 @@
-﻿copy $TABLENAME$ ($COLUMNLIST$)
+﻿begin transaction;
+
+create temp table $TABLENAME$_staging (like $TABLENAME$);
+
+copy $TABLENAME$_staging ($COLUMNLIST$)
 from 's3://$BUCKET$/$PATH$'
 credentials 'aws_access_key_id=$ACCESSKEY$;aws_secret_access_key=$SECRETKEY$'
 delimiter '$DELIMETER$'
 ignoreheader as $HEADERROWS$
 ignoreblanklines BLANKSASNULL ACCEPTANYDATE TRUNCATECOLUMNS GZIP timeformat 'auto';
+
+delete from $TABLENAME$
+using $TABLENAME$_staging
+where
+$PRIMARYKEYCHECK$;
+
+ insert into $TABLENAME$ 
+ select * from $TABLENAME$_staging;
+
+ drop table $TABLENAME$_staging;
+
+ end transaction;
